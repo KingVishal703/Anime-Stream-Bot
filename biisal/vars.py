c98b032dd18fc5e6326d646fb85644a68d19ed8e
KingVishal703 <vishal21wankhede@gmail.com>
2025-08-06 10:36:21 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -15,19 +15,19 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
+    API_ID = int(getenv('API_ID', '25452590'))
+    API_HASH = str(getenv('API_HASH', '7ccce409c7280e0153521df4458df7e3'))
     BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
     PICS = (environ.get('PICS', 'https://envs.sh/dPl.jpg')).split()
-    name = str(getenv('name', 'WeebStreamBot'))
+    name = str(getenv('name', 'AKLinks_Video_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002493002460'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002190352334'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002836031835'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002836031835'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "8371607189").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "5796857876").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'NullZoro'))
@@ -44,12 +44,13 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'AkMoviesHubBackup')) 
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Special_Dark_Videos')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.ᴄᴏɴᴛᴀᴄᴛ @Ak_ownerbot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
     SHORTLINK = is_enabled('SHORTLINK', False)
     SHORTLINK_URL = getenv('SHORTLINK_URL', '')
     SHORTLINK_API = getenv('SHORTLINK_API', '')
+
 
 
